e72a2c9bd82c30c4f295f6d94837256fab7951b0
BhavShah01 <bhaveshshah01@gmail.com>
2024-11-17 02:25:26 +0000

    app

--- inst/SaltsR_app/server.R
+++ inst/SaltsR_app/server.R
@@ -12,34 +12,34 @@ server <- function(input, output) {
   # bs_themer()
 
   output$sel_sample <- renderUI({
-    textInput("sel_sample", "Sample name", value = "Unique name to link to metadata")
+    textInput("sel_sample", "Sample name", value = "Name to link to metadata")
   })
   output$sel_dry_g <- renderUI({
-    numericInput("sel_dry_g", "Dry sample mass (g)", value = 0.801, min = 0)
+    numericInput("sel_dry_g", "Sample (g)", value = 0.801, min = 0)
   })
   output$sel_water_ml <- renderUI({
     numericInput("sel_water_ml", "Water (ml)", value = 100, min = 0)
   })
   output$sel_chloride <- renderUI({
-    numericInput("sel_chloride", "Chloride (ppm)", value = 14.651, min = 0)
+    numericInput("sel_chloride", "Cl (ppm)", value = 14.651, min = 0)
   })
   output$sel_nitrate <- renderUI({
-    numericInput("sel_nitrate", "Nitrate (ppm)", value = 17.339, min = 0)
+    numericInput("sel_nitrate", "NO3 (ppm)", value = 17.339, min = 0)
   })
   output$sel_sulfate <- renderUI({
-    numericInput("sel_sulfate", "Sulfate  (ppm)", value = 39.923, min = 0)
+    numericInput("sel_sulfate", "SO4  (ppm)", value = 39.923, min = 0)
   })
   output$sel_sodium <- renderUI({
-    numericInput("sel_sodium", "Sodium (ppm)", value = 2.027, min = 0)
+    numericInput("sel_sodium", "Na (ppm)", value = 2.027, min = 0)
   })
   output$sel_potassium <- renderUI({
-    numericInput("sel_potassium", "Potassium (ppm)", value = 2.04, min = 0)
+    numericInput("sel_potassium", "K (ppm)", value = 2.04, min = 0)
   })
   output$sel_calcium <- renderUI({
-    numericInput("sel_calcium", "calcium (ppm)", value = 49.809, min = 0)
+    numericInput("sel_calcium", "Ca (ppm)", value = 49.809, min = 0)
   })
   output$sel_magnesium <- renderUI({
-    numericInput("sel_magnesium", "Magnesium (ppm)", value = 0.581, min = 0)
+    numericInput("sel_magnesium", "Mg (ppm)", value = 0.581, min = 0)
   })
 
   salts_results <- reactive({
@@ -60,14 +60,13 @@ server <- function(input, output) {
     salts_results() |>
       select(ends_with("_x_ECOS")) |>
       DT::datatable(extensions = c("Buttons"),
-                    list(dom = 'Bt', buttons = list("copy", "excel")))
+                    list(dom = 'tB', buttons = list("copy", "excel")))
   })
-
   output$salts_wt_ECOS <- renderDT({
     salts_results() |>
       select(ends_with("_wt_ECOS")) |>
       DT::datatable(extensions = c("Buttons"),
-                    list(dom = 'Bt', buttons = list("copy", "excel")))
+                    list(dom = 'tB', buttons = list("copy", "excel")))
   })
 
   output$salts_pathway <- renderText({
--- inst/SaltsR_app/ui.R
+++ inst/SaltsR_app/ui.R
@@ -7,25 +7,20 @@ ui <- page_sidebar(
   title = "SaltsR",
   theme = bs_theme(bootswatch = "shiny"),
   sidebar = sidebar(
-    title = "Select",
+    title = "Start balance",
+    uiOutput("sel_sample"),
+    textOutput("salts_pathway"),
+    textOutput(" salts_warning"),
   ),
   card(
-    height = 350,
     full_screen = TRUE,
     card_header("Salt balance (single)"),
 
     card_body(
-     #min_height = 10,
       layout_column_wrap(
-        width = 1/5,
-        uiOutput("sel_sample"),
+        width = 1/9,
         uiOutput("sel_dry_g"),
         uiOutput("sel_water_ml"),
-        textOutput("salts_pathway"),
-        textOutput(" salts_warning")
-      ),
-      layout_column_wrap(
-        width = 1/7,
         uiOutput("sel_chloride"),
         uiOutput("sel_nitrate"),
         uiOutput("sel_sulfate"),
@@ -34,11 +29,11 @@ ui <- page_sidebar(
         uiOutput("sel_calcium"),
         uiOutput("sel_magnesium")
       ),
-      "Results",
-      "Molar inputs for ECOS Runsalt",
+      fluidRow(
+      h5("Molar inputs for ECOS Runsalt"),
       DT::dataTableOutput("salts_x_ECOS"),
-      "Weight inputs for ECOS Runsalt",
+      h5("Weight inputs for ECOS Runsalt"),
       DT::dataTableOutput("salts_wt_ECOS")
-      )
+      ))
     ),
 )
